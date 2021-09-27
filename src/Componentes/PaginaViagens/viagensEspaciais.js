ab5c6c91819efc75c391dedff21bc2ab4ea1f879
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-09-27 14:24:26 -0300

    Componente dos Produtos de Viagens Espaciais foi criado e estilizado com styled-components

--- src/Componentes/PaginaViagens/viagensEspaciais.js
+++ src/Componentes/PaginaViagens/viagensEspaciais.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import mercurio from "./images/mercurio.jpg"
 import venus from "./images/venus.jpg"
@@ -10,6 +11,30 @@ import urano from "./images/urano.jpg"
 import netuno from "./images/netuno.jpg"
 import sistemasolar from "./images/sistemasolar.jpg"
 
+const ProdutosContainer = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 10px
+` 
+const Produto = styled.div`
+    box-shadow: 1px 1px darkgray;
+    text-align: center;
+
+`
+const ImagemProduto = styled.img`
+    width: 100%;
+    height: 70%;
+`
+const NomeProduto = styled.h3`
+    text-align: center;
+`
+
+const ValorProduto = styled.p`
+    text-align: center;
+`
+
+
 
 class ViagensEspaciais extends React.Component {
     state = {
@@ -18,55 +43,55 @@ class ViagensEspaciais extends React.Component {
                 id: 1,
                 name: "Viagem em volta do Planeta Mercúrio",
                 value: 10000.00,
-                image: {mercurio}
+                image: mercurio
             },
             {
                 id: 2,
                 name: "Viagem em volta do Planeta Vênus",
                 value: 15000.00,
-                image: {venus}
+                image: venus
             },
             {
                 id: 3,
                 name: "Viagem em volta do Planeta Terra",
                 value: 20000.00,
-                image: {terra}
+                image: terra
             },
             {
                 id: 4,
                 name: "Viagem em volta do Planeta Marte",
                 value: 25000.00,
-                image: {marte}
+                image: marte
             },
             {
                 id: 5,
                 name: "Viagem em volta do Planeta Júpiter",
                 value: 35000.00,
-                image: {jupiter}
+                image: jupiter
             },
             {
                 id: 6,
                 name: "Viagem em volta do Planeta Saturno",
                 value: 45000.00,
-                image: {saturno}
+                image: saturno
             },
             {
                 id: 7,
                 name: "Viagem em volta do Planeta Urano",
                 value: 55000.00,
-                image: {urano}
+                image: urano
             },
             {
                 id: 8,
                 name: "Viagem em volta do Planeta Netuno",
                 value: 65000.00,
-                image: {netuno}
+                image: netuno
             },
             {
                 id: 9,
                 name: "Combo Completo! Viagem de Mercúrio até Netuno.",
                 value: 250000.00,
-                image: {sistemasolar}
+                image: sistemasolar
             }
 
 
@@ -75,23 +100,21 @@ class ViagensEspaciais extends React.Component {
     }
     render() {
         return(
-            <div>
-                <div>
-                    {this.state.viagens.map((iten, indice) => {
-                        return (
-                            <div key= {indice}>
-                                <img src={iten.image} alt= "Planeta"/>
-                                <h3>{iten.name}</h3>
-                                <p>R$ {iten.value}</p>
-                                <button>Adicionar ao Carrinho</button>
-                            </div>
-                        )
-                    })}
-
-                    
-                </div>
+            <ProdutosContainer>
+                              
+                {this.state.viagens.map((iten, indice) => {
+                    return (
+                        <Produto key= {indice}>
+                            <ImagemProduto src={iten.image} alt= "Planeta"/>
+                            <NomeProduto>{iten.name}</NomeProduto>
+                            <ValorProduto>R$ {iten.value}</ValorProduto>
+                            <button>Adicionar ao Carrinho</button>
+                        </Produto>
+                    )
+                })}
                 
-            </div>
+                
+            </ProdutosContainer>
         )
     }
 }
